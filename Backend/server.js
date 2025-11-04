b38b93b7160485064d740ea7509e1f82237fcec7
Akash102938 <akashjordan399@gmail.com>
2025-11-04 12:38:46 +0530

    backend configuration starting

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { connectDB } from './config/db.js';
+
+const app = express();
+const port = process.env.PORT || 4000
+
+//MIDDLEWARE
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//DATABASE
+connectDB()
+
+app.get('/', (req,res)=>{
+    res.send('API WORKING')
+})
+
+app.listen(port, ()=>{
+    console.log(`Server started on https://localhost:${port}`);
+})

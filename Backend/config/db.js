37398d913dbae9e544227a1b2ca55e88b0399d43
Akash Kumar Pandey <153708728+Akash102938@users.noreply.github.com>
2025-11-04 12:42:03 +0530

    Delete Backend directory

--- Backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://akashjordan399_db_user:mithu123@cluster0.ekaimes.mongodb.net/Foodieweb')
-    .then(()=> console.log('DB CONNECTED'))
-}

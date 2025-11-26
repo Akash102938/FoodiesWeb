9fb990b8117d8eb8b23d262fdb03d60a79018493
Akash102938 <akashjordan399@gmail.com>
2025-11-26 11:03:34 +0530

    commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export const connectDB = async () =>  {
-   const uri = process.env.MONGO_URI || 'mongodb+srv://akashjordan399_db_user:akashjordan123@cluster0.ekaimes.mongodb.net/?retryWrites=true&w=majority'
+   const uri = process.env.MONGODB_URL
 
    try {
       await mongoose.connect(uri, {
